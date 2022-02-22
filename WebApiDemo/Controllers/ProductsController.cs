1349f5da7e097bfc10240ed756af8cb717961d95
Fatih-Tarim <fatihtrm99@gmail.com>
2022-02-22 21:36:06 +0300

    Post, Put, Delete

--- WebApiDemo/Controllers/ProductsController.cs
+++ WebApiDemo/Controllers/ProductsController.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApiDemo.DataAccess;
+using WebApiDemo.Entities;
 
 namespace WebApiDemo.Controllers
 {
     [Route("api/products")]
-    public class ProductsController:Controller
+    public class ProductsController : Controller
     {
         IProductDal _productDal;
         public ProductsController(IProductDal productDal)
@@ -20,6 +21,53 @@ namespace WebApiDemo.Controllers
         {
             var products = _productDal.GetList();
             return Ok(products);
+        }
+        [HttpGet("{productId}")]
+        public IActionResult Get(int productId)
+        {
+            try
+            {
+                var product = _productDal.Get(p => p.ProductId == productId);
+                if (product == null)
+                {
+                    return NotFound($"There is no product with Id:{productId}");
+                }
+                return Ok(product);
+            }
+            catch { }
+            return BadRequest();
+        }
+        public IActionResult Post(Product product)
+        {
+            try
+            {
+                _productDal.Add(product);
+                return new StatusCodeResult(201);
+            }
+            catch { }
+            return BadRequest();
+        }
+        [HttpPut]
+        public IActionResult Put(Product product)
+        {
+            try
+            {
+                _productDal.Update(product);
+                return Ok(product);
+            }
+            catch { }
+            return BadRequest();
+        }
+        [HttpDelete("{productId}")]
+        public IActionResult Delete(int productId)
+        {
+            try
+            {
+                _productDal.Delete(new Product {ProductId=productId});
+                return Ok();
+            }
+            catch { }
+            return BadRequest();
         }
     }
 }
