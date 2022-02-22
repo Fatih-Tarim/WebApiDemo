e1df10793b11d6682acd8ab3a0912b25ae3b236f
Fatih-Tarim <fatihtrm99@gmail.com>
2022-02-22 15:48:16 +0300

    DataAccess & Entities

---
+++ WebApiDemo/DataAccess/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.Entities;
+
+namespace WebApiDemo.DataAccess
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+
+    }
+}
---
+++ WebApiDemo/DataAccess/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.Entities;
+
+namespace WebApiDemo.DataAccess
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind; Trusted_Connection=true");
+        }
+        DbSet<Product> Products { get; set; }
+    }
+}
